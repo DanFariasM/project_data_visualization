7d07c4b3d8f52b0d295f869bf1b2893d96f23bcd
DanFariasM <danfariasm@gmail.com>
2022-12-07 23:12:32 -0500

    Introduction to APIs and processing an API response.

---
+++ python_repos.py
@@ -0,0 +1,25 @@
+import requests
+
+# Make and API call and store the response.
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers = headers)
+print(f"Status code: {r.status_code}")
+
+# Store the API response in a variable.
+response_dict = r.json()
+
+# Process results.
+print(response_dict.keys())
+
+print(f"Total repositories: {response_dict['total_count']}")
+
+# Explore the information about the repositories.
+repo_dicts = response_dict["items"]
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
